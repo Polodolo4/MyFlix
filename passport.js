1c51a47704236006cbf15b7ed813f4eecc24172f
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2022-04-28 18:42:39 -0400

    prepping for heroku

--- passport.js
+++ passport.js
@@ -20,7 +20,12 @@ passport.use(new LocalStrategy({
 
     if (!user) {
       console.log('incorrect username');
-      return callback(null, false, {message: 'Incorrect username or password son!'});
+      return callback(null, false, {message: 'Incorrect username son!'});
+    }
+
+    if (!user.validatePassword(password)) {
+        console.log('incorrect password');
+        return callback(null, false, {message: 'Incorrect Password son!'});
     }
 
     console.log('finished');
