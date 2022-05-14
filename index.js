373164f1c7ebaafab1f835680cb804b90c7b5a45
Polodolo4 <86023627+Polodolo4@users.noreply.github.com>
2022-05-14 10:56:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -155,6 +155,18 @@ app.get('/movies/genre/:Name', passport.authenticate('jwt', {session: false}),
     });
   });
 
+  app.get('/users:Username', passport.authenticate('jwt', {session: false}),
+   (req, res) => {
+    Users.findOne({Username: req.params.Username})
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+  });
+
 //UPDATE SECTION
 
 app.put('/users/:Username', 
